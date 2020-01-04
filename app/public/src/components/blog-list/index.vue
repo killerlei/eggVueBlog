716b9f32dbf7d9551e9d0ac673935633974a22fb
killerlei <1543020544@qq.com>
2020-01-04 21:01:37 +0800

    add home and blog

---
+++ app/public/src/components/blog-list/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li> 博客1</li>
+      <li> 博客2</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BlogList',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hi {
+  font-size: 26px;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
